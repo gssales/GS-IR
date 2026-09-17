cc96373bab464446b4e3987b2e6add055307a2a1
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2026-09-17 15:02:25 -0300

    fix metrics script

--- metrics.py
+++ metrics.py
@@ -15,7 +15,7 @@ from PIL import Image
 import torch
 import torchvision.transforms.functional as tf
 from utils.loss_utils import ssim
-from lpips import LPIPS
+from lpipsPyTorch import lpips
 import json
 from tqdm import tqdm
 from utils.image_utils import psnr
@@ -74,7 +74,7 @@ def evaluate(model_paths):
             for idx in tqdm(range(len(renders)), desc="Metric evaluation progress"):
                 ssims.append(ssim(renders[idx], gts[idx]))
                 psnrs.append(psnr(renders[idx], gts[idx]))
-                lpipss.append(lpips_fn(gts[idx], renders[idx]))
+                lpipss.append(lpips(renders[idx], gts[idx], net_type='vgg'))
 
             print("  SSIM : {:>12.7f}".format(torch.tensor(ssims).mean(), ".5"))
             print("  PSNR : {:>12.7f}".format(torch.tensor(psnrs).mean(), ".5"))
